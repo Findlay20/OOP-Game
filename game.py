bcf4da290c01d0a1472938dfa564650e54255411
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-12-13 11:39:26 +0000

    Prettying up of text

--- game.py
+++ game.py
@@ -20,9 +20,9 @@ while True:
     mage = Character("Mage", 40, 30, 10)
     pyro = Character("Pyro", 40, 10, 30)
 
-    print(f"Welcome to Terminal Brawl! There are 5 classes to choose from with varying stats: \n {warrior} \n {tank} \n {mage} \n {pyro}")
+    print(f"\nWelcome to Terminal Brawl! There are 5 classes to choose from with varying stats: \n\n {warrior} \n {tank} \n {mage} \n {pyro} \n")
 
-    player1choice = input("Player 1! Choose your character: Warrior | Tank | Mage | Pyro \n")
+    player1choice = input("\nPlayer 1! Choose your character: Warrior | Tank | Mage | Pyro \n")
  
     match player1choice.lower():
         case "warrior":
@@ -38,7 +38,7 @@ while True:
             break
 
 
-    player2choice = input("Player 2! Choose your character: Warrior | Tank | Mage | Pyro \n")
+    player2choice = input("\nPlayer 2! Choose your character: Warrior | Tank | Mage | Pyro \n")
  
     match player2choice.lower():
         case "warrior":
@@ -54,28 +54,32 @@ while True:
             break
 
 
-    print("Player 1 has choosen", player1.name)
-    print("Player 2 has choosen", player2.name)
+    print("\n Player 1 has choosen", player1.name)
+    print("\n Player 2 has choosen", player2.name)
 
 
     while player1.HP > 0 and player2.HP > 0:
-        player1_move = input("It's Player 1's turn. Which move would you like to make? ( Attack | other options ) \n")
+        print("\n----------------------------------------------------------")
+        player1_move = input("\nIt's Player 1's turn. Which move would you like to make? ( Attack | other options ) \n")
 
         match player1_move.lower():
             case "attack":
                 player1.attack(player2)
-                print(f"Nice attack! \n Player 1 is now on {player1.HP if player1.HP > 0 else 0} health. \n Player 2 is now on {player2.HP  if player2.HP > 0 else 0} health")
-            
+                print(f"\n Nice attack! \n")
+
+        print(f" Player 1 is now on {player1.HP if player1.HP > 0 else 0} health. \n Player 2 is now on {player2.HP  if player2.HP > 0 else 0} health")
+
         if player1.HP <= 0 or player2.HP <= 0:
             break
         
-        player2_move = input("It's Player 2's turn. Which move would you like to make? ( Attack | other options ) \n")
+        player2_move = input("\n It's Player 2's turn. Which move would you like to make? ( Attack | other options ) \n")
 
         match player2_move.lower():
             case "attack":
                 player2.attack(player1)
-                print(f"Nice attack! \n Player 1 is now on {player1.HP if player1.HP > 0 else 0} health. \n Player 2 is now on {player2.HP if player2.HP > 0 else 0} health")
+                print(f"\n Nice attack!")
 
+        print(f" Player 1 is now on {player1.HP if player1.HP > 0 else 0} health. \n Player 2 is now on {player2.HP  if player2.HP > 0 else 0} health")
 
     if player1.HP > player2.HP:
         print("Congratulations, Player 1 has won the game!")
